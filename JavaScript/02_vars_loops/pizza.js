a62392d2fab2c3b2a3d82151d009311e7c5b7ee4
Shouvik1Sarkar <shouvik3501@gmail.com>
2025-02-12 23:28:52 +0530

    practice conditionals

---
+++ JavaScript/02_vars_loops/pizza.js
@@ -0,0 +1,16 @@
+let numberOfGuest = 4;
+let pizzaSize;
+//small <=2
+
+//medium <=5
+//large
+if (numberOfGuest <= 2) {
+  pizzaSize = "small";
+} else if (numberOfGuest <= 5) {
+  pizzaSize = "medium";
+} else {
+  pizzaSize = "large";
+}
+console.log(pizzaSize);
+
+//technically if-else works on greedy algorithms
