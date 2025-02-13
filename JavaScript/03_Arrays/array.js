d415671ad1258bece559a8d92db230c520daa21c
Shouvik1Sarkar <shouvik3501@gmail.com>
2025-02-14 01:16:06 +0530

    practice basic array methods

---
+++ JavaScript/03_Arrays/array.js
@@ -0,0 +1,10 @@
+let fruits = ["apple", "cherry", "banana"];
+
+let intFruits = new Array("Kiwi", "avocado", "dragon fruit");
+console.log(fruits[0]);
+console.log(fruits.length);
+fruits[0] = "blue berry";
+console.log(fruits[0]);
+// fruits.push("new item");
+console.log(fruits);
+console.log(fruits.pop());
